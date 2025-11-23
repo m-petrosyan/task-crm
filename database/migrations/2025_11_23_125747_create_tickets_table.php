0b3739738fc6aa553411b97323733c8be4b945b0
Miqayel <miqayelpetrosyan@gmail.com>
2025-11-23 18:33:06 +0400

    added migrations/models

---
+++ database/migrations/2025_11_23_125747_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\StatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            $table->string('subject');
+            $table->text('text');
+            $table->string('status')->default(StatusEnum::NEW->value);
+            $table->timestamp('manager_reply_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
